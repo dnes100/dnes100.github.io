8c29360c9742ee16bd3fc24909d1669cc387a2cc
Dinesh Hyaunmikha <dinesh.hyaunmikha@sibesonke.com>
2018-02-13 00:06:18 +0200

    typing allowed

--- js/index.js
+++ js/index.js
@@ -14,3 +14,28 @@ onMouseStop = function() {
   let led = document.getElementById('led');
   led.classList.remove('led-yellow')
 }
+
+function Dinesh() {
+  const othis = this;
+
+  this.main = document.getElementById('main');
+  this.body = document.getElementsByTagName('body')[0];
+
+  this.name = '';
+  this._chars = [];
+
+  this.body.onkeypress = function(event) {
+    if(event.key === "Enter") {
+      othis.name = othis._chars.join('');
+      othis.main.innerText = othis.name;
+      othis._chars = [];
+      return;
+    }
+
+    othis._chars.push(event.key);
+    console.log(othis._chars);
+  }
+
+}
+
+const dinesh = new Dinesh();
